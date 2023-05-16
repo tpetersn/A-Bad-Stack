c86ddfdfe0ea543f3d78547a98c3a6afdd012674
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-16 17:40:33 -0400

    chapter 2 of linked list book, up to pop

---
+++ src/first.rs
@@ -0,0 +1,41 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node { 
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),//takes whats at the head of the list (self.head) and store it in next
+            //at the same time, were going to replace the head element of our list object with Link::Empty
+        });
+        self.head = Link::More(new_node);//updates the head to be our more variant of he link which is a Box
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        match &self.head {
+            Link::Empty => None, //list is empty it returns None
+            Link::More(node) => { //if the list is not empty it returns some and the first value of the list
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+
