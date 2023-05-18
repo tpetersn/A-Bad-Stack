4671d710a3a94d338d2d2c6fed61486f5058d0eb
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-18 18:23:08 -0400

    Ch6. Starting Unsafe Rust

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
 // use std::mem;
+use std::ptr;
 
-pub struct List<'a, T> {
+
+pub struct List<T> {
     head: Link<T>,
-    tail: Option<&'a mut Node<T>>, // NEW!
+    tail: *mut Node<T>, // DANGER DANGER
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -12,53 +14,29 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
 }
-
-impl<'a, T> List<'a, T> {
+impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None, tail: None }
-    }
-
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node {
-            elem: elem,
-            // When you push onto the tail, your next is always None
-            next: None,
-        });
-
-        // swap the old tail to point to the new tail
-        //let old_tail = mem::replace(&mut self.tail, Some(new_tail));
-
-         // Put the box in the right place, and then grab a reference to its Node
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                // If the old tail existed, update it to point to the new tail
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
-            }
-            None => {
-                // Otherwise, update the head to point to it
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
-
-        self.tail = new_tail;
-    }
-
-    pub fn pop(&'a mut self) -> Option<T> {
-        // Grab the list's current head
-        self.head.take().map(|head| {
-            let head = *head;
-            self.head = head.next;
-
-            // If we're out of 'head', make sure to set the tail to 'None'.
-            if self.head.is_none() {
-                self.tail = None;
-            }
-            head.elem
-        })
+        List {head: None, tail: ptr::null_mut()}
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #[cfg(test)]
