8db2faef6ffd81b6076079090eef78f4a56a741f
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-18 11:17:36 -0400

    Ch5. Peeking

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,7 +1,9 @@
 //a bad but safe double linked list
 #![allow(dead_code)]
 use std::rc::Rc;
-use std::cell::RefCell;
+//use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
+
 
 pub struct List<T> {
     head: Link<T>,
@@ -31,17 +33,97 @@ impl<T> List<T> {
         List { head: None, tail: None}
     }
     pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
         let new_head = Node::new(elem);
         match self.head.take() {
             Some(old_head) => {
-                old_head.borrow_mut().prev = Some(new_head.clone());
-                new_head.borrow_mut().next = Some(old_head);
-                self.head = Some(new_head);
+                // non-empty list, need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
+                new_head.borrow_mut().next = Some(old_head);         // +1 old_head
+                self.head = Some(new_head);             // +1 new_head, -1 old_head
+                // total: +2 new_head, +0 old_head -- OK!
             }
             None => {
-                self.tail = Some(new_head.clone());
-                self.head = Some(new_head);
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone());     // +1 new_head
+                self.head = Some(new_head);             // +1 new_head
+                // total: +2 new_head -- OK!
             }
         }
     }
+    pub fn pop_front(&mut self) -> Option<T> {
+        // need to take the old head, ensuring it's -2
+        self.head.take().map(|old_head| {                         // -1 old
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {                               // -1 new
+                    // not emptying list
+                    new_head.borrow_mut().prev.take();            // -1 old
+                    self.head = Some(new_head);                   // +1 new
+                    // total: -2 old, +0 new
+                }
+                None => {
+                    // emptying list
+                    self.tail.take();                             // -1 old
+                    // total: -2 old, (no new)
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    } 
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+     
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
+    }
+
 }
--- target/debug/deps/lists_ex-d3d986b8b3d53e0b.d
+++ target/debug/deps/lists_ex-d3d986b8b3d53e0b.d
@@ -1,8 +1,9 @@
-C:\Users\Thomas Petersen\Desktop\Rust_Projects\lists-ex\target\debug\deps\lists_ex-d3d986b8b3d53e0b.exe: src\lib.rs src\first.rs src\second.rs src\third.rs
+C:\Users\Thomas Petersen\Desktop\Rust_Projects\lists-ex\target\debug\deps\lists_ex-d3d986b8b3d53e0b.exe: src\lib.rs src\first.rs src\second.rs src\third.rs src\fourth.rs
 
-C:\Users\Thomas Petersen\Desktop\Rust_Projects\lists-ex\target\debug\deps\lists_ex-d3d986b8b3d53e0b.d: src\lib.rs src\first.rs src\second.rs src\third.rs
+C:\Users\Thomas Petersen\Desktop\Rust_Projects\lists-ex\target\debug\deps\lists_ex-d3d986b8b3d53e0b.d: src\lib.rs src\first.rs src\second.rs src\third.rs src\fourth.rs
 
 src\lib.rs:
 src\first.rs:
 src\second.rs:
 src\third.rs:
+src\fourth.rs:
